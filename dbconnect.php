60bcf92ebe8a00ddd45763d631a75ff1c16063c0
JATIN PATEL <130120406+JATINPATEL94@users.noreply.github.com>
2023-07-17 21:12:49 +0530

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "528926";
+$password = "admin.jobNexus";
+$database = "528926";
+
+$conn = mysqli_connect($servername, $username,$password, $database);
+if ($conn) {
+    echo "<script>
+        console.log('Connection established');
+    </script>";
+}
+if (!$conn) {
+    echo "<script>
+    alert('We are unable to connect to our database.');
+  </script>";
+}
+?>
---
+++ script.js
@@ -0,0 +1,8 @@
+// filter-section start hear //
+function applyFilters() {
+    var category = document.getElementById("category").value;
+    var location = document.getElementById("location").value;
+    var type = document.getElementById("type").value;
+    var salary = document.getElementById("salary").value;
+  }
+  
