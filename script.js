c431410a7d9928161a121c3b10e2432d25b7e1b0
Vedansh <123vedmaheshwari123@gmail.com>
2023-07-18 20:44:19 +0530

    Improved the colors

--- script.js
+++
@@ -1,8 +0,0 @@
-// filter-section start hear //
-function applyFilters() {
-    var category = document.getElementById("category").value;
-    var location = document.getElementById("location").value;
-    var type = document.getElementById("type").value;
-    var salary = document.getElementById("salary").value;
-  }
-  
